523404e67ae615793d2a31ae2793235672bd2714
BelraQ <bellrara0173@gmail.com>
2023-09-26 21:16:51 +0900

    start publishing blog page(ssr-recipe)

---
+++ learn-react-book/ssr-recipe/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from '../node_modules/react-router-dom/dist/index';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<></>}/>
+    </Routes>
+  );
+}
+
+export default App;
--- learn-react-book/ssr-recipe/src/index.js
+++ learn-react-book/ssr-recipe/src/index.js
@@ -4,28 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import rootReducer from './modules';
-import { applyMiddleware, createStore } from 'redux';
-import { createLogger } from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-const logger = createLogger();
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(logger)),
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
